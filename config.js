e80352a7f011866c7ca83bb59e119b58c4958b78
Gaëtan Haefeli <gagah98@gmail.com>
2019-10-14 15:29:57 +0200

    changement config

--- config.js
+++ config.js
@@ -1,2 +1,22 @@
+try {
+    require('dotenv').config();
+  } catch (err) {
+    console.log('No .env file loaded');
+  }
+
 exports.port = process.env.PORT || 3000;
 exports.secretKey = process.env.MY_APP_SECRET_KEY || 'changeme';
+
+// Validate that port is a positive integer.
+if (process.env.PORT) {
+    const parsedPort = parseInt(process.env.PORT, 10);
+    if (!Number.isInteger(parsedPort)) {
+      throw new Error('Environment variable $PORT must be an integer');
+    } else if (parsedPort < 1 || parsedPort > 65535) {
+      throw new Error('Environment variable $PORT must be a valid port number');
+    }
+  }
+  // Validate that some environment variable is set.
+  if (!process.env.MY_APP_FOO) {
+    throw new Error('Environment variable $MY_APP_FOO must be set');
+  }
