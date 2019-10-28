6bef50f4dc9e8407ebd27c2812c400f23a74257b
Gaëtan Haefeli <gagah98@gmail.com>
2019-10-28 15:29:55 +0100

    ajour bcrypt + login (non fonctionnel)

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  User.find().sort('name').exec(function(err, users) {
+router.get('/', function (req, res, next) {
+  User.find().sort('name').exec(function (err, users) {
     if (err) {
       return next(err);
     }
@@ -13,18 +14,49 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST new user */
-router.post('/', function(req, res, next) {
-  // Create a new document from the JSON in the request body
-  const newUser = new User(req.body);
-  // Save that document
-  newUser.save(function(err, savedUser) {
+router.post('/', function (req, res, next) {
+
+  //encrypt the password
+
+  const plainPassword = 'changeme';
+  const saltRounds = 10;
+  bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+
+    // Create a new document from the JSON in the request body
+    const newUser = new User(req.body);
+    newUser.password = hashedPassword;
+    // Save that document
+    newUser.save(function (err, savedUser) {
+      if (err) {
+        return next(err);
+      }
+      // Send the saved document in the response
+      console.log(savedUser)
+      res.send(savedUser);
+    });
+  });
+});
+
+/* LOGIN user */
+router.post('/login', function (req, res, next) {
+  User.findOne({ username: req.body.username }).exec(function (err, user) {
     if (err) {
       return next(err);
+    } else if (!user) {
+      return res.sendStatus(401);
+      
     }
-    // Send the saved document in the response
-    console.log(savedUser)
-    res.send(savedUser);
-  });
+    bcrypt.compare(req.body.password, user.password, function (err, valid) {
+      if (err) {
+        return next(err);
+      } else if (!valid) {
+        console.log("pas bon");
+        return res.sendStatus(401);
+      }
+      // Login is valid...
+      res.send(`Welcome ${user.username}!`);
+    });
+  })
 });
 
 module.exports = router;
