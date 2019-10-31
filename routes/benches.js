99667f36b7738d1b4e14275a396ef5e55145fe30
loic.postec <loic.postec@heig-vd.ch>
2019-10-31 10:37:46 +0100

    add bench deletation

--- routes/benches.js
+++ routes/benches.js
@@ -24,12 +24,14 @@ router.post('/', function(req, res, next) {
       res.send(savedBench);
     });
   });
-  /*router.deleteModel('/:id', function(req, res, next) {
+  router.deleteModel('/:id', function(req, res, next) {
+    const id = req.params.id;
+    Bench.deleteOne(ObjectId(id));
       if (err) {
         return next(err);
       }
       // Send the saved document in the response
       res.send(savedBench);
   });
-  */
+  
   module.exports = router;
