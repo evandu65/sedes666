0daae15a836a412801e32cc813ccf5f9ad5ab3cc
loic.postec <loic.postec@heig-vd.ch>
2019-11-07 09:08:35 +0100

    fix object id

--- routes/benches.js
+++ routes/benches.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Bench = require('../models/bench');
+const ObjectId = mongoose.Types.ObjectId;
 
 /* GET benches listing. */
 router.get('/', function (req, res, next) {
