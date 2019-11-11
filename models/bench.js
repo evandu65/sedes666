dacb71de32b557eebf7838f3e9151850aaa2bd5a
Evan Vecchio <evan.vecchio@heig-vd.ch>
2019-11-11 15:38:14 +0100

    Adding countBench on user

--- models/bench.js
+++ models/bench.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const integerValidator = require('mongoose-integer');
+const ObjectId = mongoose.Types.ObjectId;
 
 const benchSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required:true,
+    validate: {
+      // Validate that the userId is a valid ObjectId
+      // and references an existing person
+      validator: validateUserId,
+      message: function(props) { return props.reason.message; }
+    }
+  },
     description: String,
     creationDate: { type: Date, default: Date.now  }, // Default value
     modifDate: { type: Date, default: Date.now  }, // Default value
@@ -32,6 +43,26 @@ const benchSchema = new Schema({
   function validateGeoJsonCoordinates(value) {
     return Array.isArray(value) && value.length >= 2 && value.length <= 3 && value[0] >= -180 && value[0] <= 180 && value[1] >= -90 && value[1] <= 90;
   }
+
+  function validateUserId(value) {
+    console.log('Validating user ID');
+    return new Promise((resolve, reject) => {
+  
+      if (!ObjectId.isValid(value)) {
+        throw new Error(`validateUserId is not a valid user reference`);
+      }
+  
+      mongoose.model('User').findOne({ _id: ObjectId(value) }).exec()
+        .then((user) => {
+          if (!user) {
+            throw new Error(`validateUserId does not reference a user that exists`);
+          } else {
+            resolve(true);
+          }
+        })
+        .catch(e => { reject(e) });
+    })  
+  };
 
 
   module.exports = mongoose.model('Bench', benchSchema);
