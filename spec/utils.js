f858ffc873983c53d6c5f6cff9a4b9c2faa63798
Gaëtan Haefeli <gagah98@gmail.com>
2019-11-16 13:10:16 +0100

    fix automated test with auth

--- spec/utils.js
+++ spec/utils.js
@@ -1,5 +1,9 @@
 const User = require('../models/user');
 const Bench = require('../models/bench');
+const supertest = require('supertest');
+const app = require('../app');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY || 'changeme';
 
 exports.cleanUpDatabaseUser = async function() {
   await Promise.all([
